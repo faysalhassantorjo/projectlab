3da254e42818b8a8919e27749e7ca02e3b6cb720
faysal <faysalhassantorjo8@gmail.com>
2025-03-13 01:52:46 +0600

    static

--- projectlab/settings.py
+++ projectlab/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
