1df4395c4536375c2b406cba915ac66dbb9b012b
torjo <faysalhassantorjo8@gmail.com>
2025-06-20 23:16:30 +0600

    feat: Update STATICFILES_DIRS and STATIC_ROOT settings based on DEBUG mode

--- projectlab/settings.py
+++ projectlab/settings.py
@@ -155,10 +155,12 @@ USE_TZ = True
 
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
